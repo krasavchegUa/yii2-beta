6b098ea2c23bd783efd85176082d51b93db87b7d
krasavchegUa <krasavcheg.ua@gmail.com>
2014-04-28 23:01:43 +0300

    http://www.ng-newsletter.com/posts/beginner2expert-directives.html

--- views/angular/index.php
+++ views/angular/index.php
@@ -11,9 +11,33 @@ $this->registerJsFile('/js/ng-script.js');
     <div data-ng-controller="PlayerController">
         <button data-ng-click="play()" class="btn btn-success" data-ng-show="!playing">Play</button>
         <button data-ng-click="pause()" class="btn btn-warning" data-ng-show="playing">Pause</button>
-        <button data-ng-click="stop()" class="btn btn-danger" data-ng-show="playing">Stop</button>
+        <button data-ng-click="stop()" class="btn btn-danger" data-ng-show="playing || audio.currentTime">Stop</button>
         <br/>
         Playing audio: <b>{{ playing }}</b>
     </div>
 
+    <hr/>
+
+    <div data-ng-controller="MyController">
+        <input type="text" data-ng-model="person.name" class="form-control" placeholder="Enter your name" />
+        <h5>Hello {{ person.name }}</h5>
+        <h5>{{ clock }}</h5>
+    </div>
+
+    <hr/>
+
+    <div data-ng-controller="DemoController">
+        <h4>The simplest adding machine ever</h4>
+        <button data-ng-click="add(3)" class="btn btn-primary">Add</button>
+        <button data-ng-click="subtract(2)" class="btn btn-default">Subtract</button>
+        <h4>Current count: {{ counter }}</h4>
+    </div>
+
+    <hr/>
+
+    <div data-ng-controller="ListController">
+        <pre data-ng-repeat="prog in programs">
+            {{prog}}
+        </pre>
+    </div>
 </div>
--- web/js/ng-script.js
+++ web/js/ng-script.js
@@ -1,5 +1,53 @@
 var app = angular.module('exterzoApp', []);
 
+app.controller('MyController', function($scope) {
+    $scope.person = { name: "Rudak Volodymyr" };
+    var updateClock = function() {
+        $scope.clock = new Date();
+    };
+    var timer = setInterval(function() {
+        $scope.$apply(updateClock);
+    }, 1000);
+    updateClock();
+});
+
+app.controller('DemoController', function($scope){
+    $scope.counter = 0;
+    $scope.add = function(amount) {
+        $scope.counter += amount;
+    };
+    $scope.subtract = function(amount){
+        $scope.counter -= amount;
+        if($scope.counter < 0 ){
+            $scope.counter = 0;
+        }
+    }
+});
+
+app.controller('ListController', function($scope, $http) {
+    var apiKey = 'MDEzNjU2OTcxMDEzOTg3MDgzNDQzYjIwZA001',
+        nprUrl = 'http://api.npr.org/query?id=61&fields=relatedLink,title,byline,text,audio,image,pullQuote,all&output=JSON';
+    // Hidden our previous section's content
+    // construct our http request
+    $http({
+        method: 'JSONP',
+        url: nprUrl + '&apiKey=' + apiKey + '&callback=JSON_CALLBACK'
+    }).success(function(data, status) {
+            $scope.programs = data.list.story;
+            console.log(data);
+            // Now we have a list of the stories (data.list.story)
+            // in the data object that the NPR API
+            // returns in JSON that looks like:
+            // data: { "list": {
+            //   "title": ...
+            //   "story": [
+            //     { "id": ...
+            //       "title": ...
+        }).error(function(data, status) {
+            // Some error occurred
+        });
+});
+
 app.controller('PlayerController', ['$scope', function($scope) {
     $scope.playing = false;
     $scope.audio = document.createElement('audio');
